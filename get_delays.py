89253a208b065a8ea4e67964d1c369b44bbfb5b6
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-01-27 19:56:58 -0500

    analysis of delayed flights

--- get_delays.py
+++ get_delays.py
@@ -13,11 +13,26 @@ df=pd.DataFrame(rows,columns=[desc.name for desc in cursor.description])
 cursor.close()
 #print(df.head()) check if data is in df
 
-cleaned_df=df.dropna(subset=["arr_del15","dep_del15"])
+#cleaned_df=df[df["arr_del15","dep_del15"].isna()]
+cleaned_df=df.dropna(subset=['arr_del15','dep_del15'])
+
 #no null values print(len(cleaned_df))
 
-#create new column with delayed flights
+#GENERATE NEW COL with delayed flights
+df["delayed"]=np.where((df["arr_del15"]=='1') | (df["dep_del15"]=='1'),1,0)
+#print(df["delayed"])
 
-df['delayed'] = np.where((df['arr_del15'] == df['dep_del15']), np.where(df['arr_del15']==True,1,0), np.nan)
+#NEW DF TO GROUP AIRLINES, AIRPORTS
+#and save as CSV
 
-print(df["delayed"])
+airline_delays=df.groupby("op_unique_carrier")["delayed"].mean()
+airline_delays.sort_values(inplace=True,ascending=False)
+#print(airline_delays)
+airline_delays.to_csv("airline_delays_jan.csv")
+airport_delays=df.groupby("origin_airport_id")["delayed"].mean()
+airport_delays.sort_values(inplace=True,ascending=False)
+#print(airport_delays)
+airport_delays.to_csv("airport_delays_jan.csv")
+
+
+
