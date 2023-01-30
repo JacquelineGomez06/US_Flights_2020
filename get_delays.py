9ac567291ac33fd38c6d03999e154b8631c3dc93
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-01-30 18:50:09 -0500

    r2 score calcultion

--- get_delays.py
+++ get_delays.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 import numpy as np
 from config import params_dic
-
+from sklearn.metrics import r2_score
 
 conn = psycopg2.connect(**params_dic)
 cursor = conn.cursor()
@@ -28,11 +28,22 @@ df["delayed"]=np.where((df["arr_del15"]=='1') | (df["dep_del15"]=='1'),1,0)
 airline_delays=df.groupby("op_unique_carrier")["delayed"].mean()
 airline_delays.sort_values(inplace=True,ascending=False)
 #print(airline_delays)
-airline_delays.to_csv("airline_delays_jan.csv")
+#airline_delays.to_csv("airline_delays_jan.csv")
 airport_delays=df.groupby("origin_airport_id")["delayed"].mean()
 airport_delays.sort_values(inplace=True,ascending=False)
 #print(airport_delays)
-airport_delays.to_csv("airport_delays_jan.csv")
+#airport_delays.to_csv("airport_delays_jan.csv")
+
+#COMPARE AIRLINES TO JD RATING
+#import jd ratings
+jd_ratings=pd.read_csv("/Users/Jackie/jd_ratings.csv")
+
+joined_ratings=pd.merge(airline_delays,jd_ratings,how='right',left_on=['op_unique_carrier'],right_on=['code'])
+joined_ratings.dropna(subset='delayed',inplace=True)
+
+x=joined_ratings["rating"]
+y=joined_ratings["delayed"]
 
 
-
+r2=r2_score(x,y)
+print(r2)
